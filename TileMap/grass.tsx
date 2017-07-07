e115c28c2f4a0a2d17fb6b45121239d496b8355a
xuan hien <xuanhienhaihau@gmail.com>
2017-07-07 09:11:57 +0700

    Stage 1-1

---
+++ TileMap/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="grass.png" width="32" height="64"/>
+</tileset>
--- state1.js
+++ state1.js
@@ -1,10 +1,22 @@
-
-demo.state1 = function(){};
+demo.state1 = function () {};
 demo.state1.prototype = {
-    preload: function(){},
-    create: function(){
+    preload: function () {
+        //        game.load.tilemap('field','TileMap/map.json',null,Phaser.Tilemap.TILED_JSON);
+        game.load.tilemap('field', 'TileMap/map.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('grassTiles', 'TileMap/grass.png');
+        game.load.image('rockTiles', 'TileMap/rock.png');
+    },
+    create: function () {
         game.stage.backgroundColor = '#DDDDDD';
         addChangeStateEventListeners();
+
+        var map = game.add.tilemap('field');
+        //        map.addTilesetImage('grassTiles');
+        //        map.addTilesetImage('rockTiles');
+        //
+        //        var grass1 = map.createLayer('grass1');
+        //        var rock1 = map.createLayer('rock1');
+
     },
-    update: function(){}
+    update: function () {}
 };
